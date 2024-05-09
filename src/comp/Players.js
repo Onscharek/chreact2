bc753643c0384278b4cd5025af2a51d76984f283
Onscharek <158042832+Onscharek@users.noreply.github.com>
2024-05-09 17:54:42 +0100

    Add files via upload

---
+++ src/comp/Players.js
@@ -0,0 +1,43 @@
+const Players =[
+{
+    name: <h1> "Lionel Messi" </h1>,
+    team: "Argentina",
+    natinality: <h2>"tunisian"</h2>,
+    jerseyNumber: "28",
+    age:"20",
+    Img_URL:"https://assets.goal.com/images/v3/bltd58c4d60ecd9275e/GOAL_-_Blank_WEB_-_Facebook_-_2023-06-13T135350.847.png?auto=webp&format=pjpg&width=1080&quality=60"
+
+},
+{
+    name: <h1> "Mbappé" </h1>,
+    team: "	Paris Saint-Germain ",
+    natinality: <h2>"tunisian"</h2>,
+    jerseyNumber: "8",
+    age:"30",
+    Img_URL:"https://media.guim.co.uk/abadabb4475f98e5e4d3b484f1c93ff042b1c8ba/0_95_2742_1645/500.jpg"
+
+},
+{
+    name: <h1> "Benzema" </h1>,
+    team: "Real Madrid",
+    natinality: <h2>"tunisian"</h2>,
+    jerseyNumber: "12",
+    age:"25",
+    Img_URL:"https://media.guim.co.uk/65d111ee08979d1ad86d06816b93573b8ed57525/0_88_5440_3263/500.jpg"
+
+},
+{
+    name: <h1> "Luka Modric" </h1>,
+    team: "	Real Madrid ",
+    natinality: <h2>"tunisian"</h2>,
+    jerseyNumber: "2",
+    age:"27/",
+    Img_URL:"https://media.guim.co.uk/2de34801abc8d247bfc7b2d7f4fad5ed2277af27/0_177_2800_1681/500.jpg"
+
+}
+
+
+]
+
+// Export the product object as JSON
+export default Players 
